88a465b1e050be4367a8ee5083067962b9887611
Faye Skeen <fskeen@gmail.com>
2020-02-15 15:05:02 -0800

    another str reverse solution + 2 palindrome solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,15 +7,23 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+    debugger;
     // using native array methods
-    let reversed = [...str].reverse().join('')
+    // let reversed = [...str].reverse().join('')
 
-    // using a loop to iterate using string attributes
+    // using newer for...of loop syntax 
+    // let loopReversed = ''
+    // for (let character of str) {
+    //     loopReversed = character + loopReversed; // adds the current character onto the START of the reversed string
+    // }
+
+    // using classic for loop to iterate using string attributes
     let old = []
-    for (let i = str.length; i >= 0; i--) {
+    for (let i = str.length-1; i >= 0; i--) {
         old.push(str[i])
     }
     return old.join('')
-}
 
+}
+reverse('asdf')
 module.exports = reverse;
