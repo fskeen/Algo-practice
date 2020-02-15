b7c7836630d01ee4a914c73c1d1e585acebf4488
Faye Skeen <fskeen@gmail.com>
2020-02-15 14:23:46 -0800

    reverse string done w 2 solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // using native array methods
+    let reversed = [...str].reverse().join('')
+
+    // using a loop
+    let old = []
+    for (let i = str.length; i >= 0; i--) {
+        old.push(str[i])
+    }
+    return old.join('')
+}
 
 module.exports = reverse;
