f6227d04c581e662a0039e54bb2005c895d053ed
Faye Skeen <fskeen@gmail.com>
2020-02-15 15:16:54 -0800

    palidrone solution with .every()

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,22 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+
+    // comparing reversed string
+        // const rev = [...str].reverse().join('')
+        // return str === rev 
+
+    // comparing halves
+        // const halfA = str.slice(0, Math.floor((str.length/2)))
+        // const halfB = str.slice(Math.ceil((str.length/2)), str.length)
+
+        // return halfA === [...halfB].reverse().join('')
+
+    // using .every()
+        return str.split('').every((char, i) => {
+            return char === str[str.length-1-i]
+        })
+}
 
 module.exports = palindrome;
