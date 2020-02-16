88da558d0e3df4451e767b6a80d12eb894d5fd20
Faye Skeen <fskeen@gmail.com>
2020-02-16 13:23:00 -0800

    complete solution to maxChar using reduce + comments

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+function maxChar(str) {
+    let obj = {};
+    // [...str].forEach((character, i) => {
+    //     obj = {
+    //         ...obj,
+    //         str[i]: character
+    //     }
+    // })
+
+    for (let char of str) {
+        obj[char] = obj[char] + 1 || 1
+        // This is creating an object out of the string and recording the number of instances any character appears. It's saying "If there's a value at the key of char, increment by 1; otherwise, create that key and set its value to 1"
+    }
+    return Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b)
+    // This turns the object into an array of keys, which we then iterate through with reduce. Reduce takes two keys and compares the value of object[keyA] against object[keyB], eventually returning the single highest value due to the ternary statement. This MAY not work if there are multiple highest values? Would need to probably use a diff array method or implement some control flow in the reduce for when obj[a] and obj[b] are equal.
+}
 
 module.exports = maxChar;
