a7218ac8ecca180a8d3249c51d905777b4ec734e
Faye Skeen <fskeen@gmail.com>
2020-02-19 18:18:11 -0800

    figured out BETTER anagram solution!

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,45 +8,70 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function makeArr (string) {
 
-    const obj = {}
-    for (let char of string) {
-          if (obj[char]) {
-            obj[char]++
-          } else {
-            obj[char.toLowerCase()] = 1
-          }
-      }
-    return Object.entries(obj).sort()
-   
-  }
-  
+  // realized I didn't need to make an object at all and could just use strings and arrays! TAKE TWO:
   function anagrams(stringA, stringB) {
-    // clean the strings of any punctuation
-    const cleanStringA = stringA.match(/\w/gi).join('').toLowerCase()
-    const cleanStringB = stringB.match(/\w/gi).join('').toLowerCase()
-    // if the cleaned strings are different lengths, they're not anagrams, no further checking needed
-    if (cleanStringA.length !== cleanStringB.length) {
-      return false
-    }
-    // use a function to make a sorted array of letters
-    const arrA = makeArr(cleanStringA)
-    const arrB = makeArr(cleanStringB)
+    // clean the strings of any punctuation, make them lowercase, then sort and join the array into a string
+    const cleanStringA = stringA.toLowerCase().match(/\w/gi).sort().join()
+    const cleanStringB = stringB.toLowerCase().match(/\w/gi).sort().join()
+
     // default state is true because one false letter means they aren't anagrams, while one true letter doesn't necessarily mean they ARE anagrams. We're checking for false letters to see if we should flip the boolean to false.
     let isAnagram = true
-    if (arrA.length !== arrB.length) {
-        // checking for length of combined array -- if unequal, they are not anagrams
+    if (cleanStringA.length !== cleanStringB.length) {
+        // if the cleaned strings are different lengths, they're not anagrams, no further checking needed
         isAnagram = false
       } else {
-        arrA.forEach((item, i) => {
-        // console.log(item[1])
-          if ((item[1] !== arrB[i][1])) {
-            anagram = false
+          // since we sorted the string and we know the strings are the same length, we can check for equality character by character. if any don't match, it's not an anagram
+        for (let i = 0; i < cleanStringA.length; i++) {
+            if (cleanStringA[i] !== cleanStringB[i])
+            isAnagram = false
           }
-        })
       }
     return isAnagram
   }
 
 module.exports = anagrams;
+
+
+// THE FIRST SOLUTION I CAME UP WITH:
+
+// function makeArr (string) {
+// a helper function that extracts the "make an object containing characters and counts" logic away from my anagrams function
+//     const obj = {}
+//     for (let char of string) {
+//           if (obj[char]) {
+//             obj[char]++
+//           } else {
+//             obj[char.toLowerCase()] = 1
+//           }
+//       }
+//     return Object.entries(obj).sort()
+   
+//   }
+
+// function anagrams(stringA, stringB) {
+//     // clean the strings of any punctuation
+//     const cleanStringA = stringA.match(/\w/gi).join('').toLowerCase()
+//     const cleanStringB = stringB.match(/\w/gi).join('').toLowerCase()
+//     // if the cleaned strings are different lengths, they're not anagrams, no further checking needed
+//     if (cleanStringA.length !== cleanStringB.length) {
+//       return false
+//     }
+//     // use a function to make a sorted array of letters
+//     const arrA = makeArr(cleanStringA)
+//     const arrB = makeArr(cleanStringB)
+//     // default state is true because one false letter means they aren't anagrams, while one true letter doesn't necessarily mean they ARE anagrams. We're checking for false letters to see if we should flip the boolean to false.
+//     let isAnagram = true
+//     if (arrA.length !== arrB.length) {
+//         // checking for length of combined array -- if unequal, they are not anagrams
+//         isAnagram = false
+//       } else {
+//         arrA.forEach((item, i) => {
+//         // console.log(item[1])
+//           if ((item[1] !== arrB[i][1])) {
+//             anagram = false
+//           }
+//         })
+//       }
+//     return isAnagram
+//   }
