4fb658023d3064069d18f787f6e5c9e863f5c7d1
Faye Skeen <fskeen@gmail.com>
2020-02-19 17:40:48 -0800

    solution to check for anagrams w regex, loops, control flow

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function makeArr (string) {
+
+    const obj = {}
+    for (let char of string) {
+          if (obj[char]) {
+            obj[char]++
+          } else {
+            obj[char.toLowerCase()] = 1
+          }
+      }
+    return Object.entries(obj).sort()
+   
+  }
+  
+  function anagrams(stringA, stringB) {
+    // clean the strings of any punctuation
+    const cleanStringA = stringA.match(/\w/gi).join('').toLowerCase()
+    const cleanStringB = stringB.match(/\w/gi).join('').toLowerCase()
+    // if the cleaned strings are different lengths, they're not anagrams, no further checking needed
+    if (cleanStringA.length !== cleanStringB.length) {
+      return false
+    }
+    // use a function to make a sorted array of letters
+    const arrA = makeArr(cleanStringA)
+    const arrB = makeArr(cleanStringB)
+    // default state is true because one false letter means they aren't anagrams, while one true letter doesn't necessarily mean they ARE anagrams. We're checking for false letters to see if we should flip the boolean to false.
+    let isAnagram = true
+    if (arrA.length !== arrB.length) {
+        // checking for length of combined array -- if unequal, they are not anagrams
+        isAnagram = false
+      } else {
+        arrA.forEach((item, i) => {
+        // console.log(item[1])
+          if ((item[1] !== arrB[i][1])) {
+            anagram = false
+          }
+        })
+      }
+    return isAnagram
+  }
 
 module.exports = anagrams;
